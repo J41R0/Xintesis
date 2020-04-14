022f23d0e54f1566de5114d9b142ef99e4f903a0
yodax <jairo.lefebre@gmail.com>
2020-04-14 11:29:49 -0400

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='xintesis',
-    version='1.0',
+    version='0.1',
     scripts=[],
     author="Jairo Lefebre",
     author_email="jairo.lefebre@gmail.com",
